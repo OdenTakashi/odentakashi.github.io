385d0e04a5aa6d28b4049604d59e825505b633d4
naoki.kodama <odentakashidesu2@gmail.com>
2025-02-06 22:34:34 +0900

    Added a script to automatically create files.

---
+++ create_post.rb
@@ -0,0 +1,20 @@
+TITLE = ARGV[0]
+CREATED_DATE = Time.now.strftime("%Y-%m-%d %H:%M")
+TODAY = Time.now.strftime("%Y-%m-%d")
+FILENAME = "_posts/#{TODAY}.md"
+
+if File.exist?(FILENAME)
+  puts "#{FILENAME} already exists."
+  return
+end
+
+File.open(FILENAME, "w") do |f|
+  f.puts <<~EOS
+    ---
+    layout: post
+    title: "#{TITLE}"
+    image: https://odentakashi.github.io/public/images/mini_annotaterb_schematic.png
+    date: #{CREATED_DATE}
+    ---
+  EOS
+end
